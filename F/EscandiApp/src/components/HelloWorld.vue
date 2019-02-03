bea166a7564a457f303ad706a1a824db384565d0
Alejandro Escandon <necro@necro.lan>
2019-02-03 12:01:46 -0600

    basic app

---
+++ F/EscandiApp/src/components/HelloWorld.vue
@@ -0,0 +1,74 @@
+<template>
+  <div>
+    <nav class="navbar is-primary is-bold" role="navigation" aria-label="main navigation">
+  <div class="navbar-brand">
+    <a class="navbar-item" href="https://pornhub.com">
+      <img src="https://upload.wikimedia.org/wikipedia/commons/f/f1/Pornhub-logo.svg" alt="Bulma: Free, open source, & modern CSS framework based on Flexbox" width="112" height="28">
+    </a>
+
+    <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false">
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+    </a>
+    <a class="navbar-item">
+           Home
+      </a>
+      <a class="navbar-item">
+           Contanto
+      </a>
+      <a class="navbar-item">
+           Sound
+      </a>
+  </div>
+</nav>
+
+<section class="hero  is-primary">
+  <div class="hero-body">
+    <div class="container">
+      <h1 class="title">
+        Alejandro Escandon
+      </h1>
+      <h2 class="subtitle">
+        Master developer
+      </h2>
+    </div>
+  </div>
+</section>
+  <info/>
+</div>
+
+</template>
+
+<script>
+import info from './info'
+export default {
+  name: 'HelloWorld',
+  components: {
+  info
+  },
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ F/EscandiApp/src/components/info.vue
@@ -0,0 +1,34 @@
+<template>
+  
+  <div>
+      <p class="title is-1 is-spaced">
+      Bienvenido a la Jungla
+      </p>
+
+      <div class="card color='primary'">
+  <div class="card-content">
+    <p class="title">
+      “¿Cree usted que yo cuento los días? Únicamente queda un día, uno que siempre se repite. Se nos da al amanecer y se nos quita al atardecer.”
+    </p>
+    <p class="subtitle">
+      Jean Paul Sartre
+    </p>
+  </div>
+</div>
+  </div>
+
+</template>
+
+
+<script>
+export default {
+}
+</script>
+
+<style>
+.card{
+  color: #000
+}
+  
+</style>
+
